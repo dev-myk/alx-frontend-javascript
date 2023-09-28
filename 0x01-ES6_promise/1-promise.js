02a465b41a9768bcff6e869a61c2c32cb09bfdd7
dev-myk <devmikekenya@gmail.com>
2023-09-28 01:06:10 +0000

    fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,7 +2,7 @@ export default function (boolean) {
   return new Promise((resolve, reject) => {
     const object = {
       status: 200,
-      body: 'success',
+      body: 'Success',
     };
 
     if (boolean === true) {
