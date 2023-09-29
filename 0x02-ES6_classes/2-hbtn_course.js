3cffd70f26ce65eeadaa8472fa1aa85fd81aa1a1
dev-myk <devmikekenya@gmail.com>
2023-09-29 01:42:33 +0000

    class implement method

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -20,29 +20,4 @@ export default class HolbertonCourse {
   get length() {
     return this._length;
   }
-
-  get students() {
-    return this._students;
-  }
-
-  set name(name) {
-    if (typeof name != 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = name;
-  }
-
-  set length(length) {
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = length;
-  }
-
-  set students(students) {
-    if (!Array.isArray(students)) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._students = students;
-  }
 }
